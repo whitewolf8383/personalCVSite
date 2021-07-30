53fe10ff0525297948014f09e573ef0f76489026
whitewolf8383 <kandt.vandf@gmail.com>
2021-07-29 23:20:22 -0600

    site skeleton build add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutSectionComponent } from './about-section/about-section.component';
+import { PortfolioSectionComponent } from './portfolio-section/portfolio-section.component';
+import { ContactSectionComponent } from './contact-section/contact-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    NavbarComponent,
+    AboutSectionComponent,
+    PortfolioSectionComponent,
+    ContactSectionComponent
   ],
   imports: [
     BrowserModule
